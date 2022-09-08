cdceacb19e42f510908b9cae29c34e7db7dc2b4c
pa-purpura <p.purpura@outlook.fr>
2022-09-08 09:44:49 +0200

    product & category items added

---
+++ database/migrations/2022_09_08_070850_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Category;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->float('price', 5, 2);
+            // $table->foreignId('user_id');
+            $table->foreignIdFor(Category::class);
+            // $table->unsignedBigInteger('category_id');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
